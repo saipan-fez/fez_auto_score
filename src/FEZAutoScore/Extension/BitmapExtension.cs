7f006f23785f56a44c2c975032e42e0d7ea87136
umyuu <124dtiaka@gmail.com>
2018-07-27 20:16:30 +0900

    AverageHash

--- src/FEZAutoScore/Extension/BitmapExtension.cs
+++ src/FEZAutoScore/Extension/BitmapExtension.cs
@@ -1,7 +1,9 @@
-﻿using System.Drawing;
+﻿using System.Collections;
+using System.Drawing;
 using System.Drawing.Imaging;
 using System.Runtime.InteropServices;
 using System.Security.Cryptography;
+using System.Linq;
 
 namespace FEZAutoScore.Extension
 {
@@ -24,7 +26,37 @@ namespace FEZAutoScore.Extension
 
             return ret;
         }
-
+        
+        public static BitArray GenerateAverageHash(this Bitmap bitmap)
+        {
+            /*
+             * 処理フロー
+             * @see http://www.hackerfactor.com/blog/index.php?/archives/432-Looks-Like-It.html
+             * 1. 画像をグレースケール
+             * 2, グレースケール画像の平均値を取得
+             * 3, ハッシュ化
+             */
+            byte[] grayscale = new byte[bitmap.Width * bitmap.Height];
+            // グレースケール
+            for (int x = 0; x < bitmap.Width; x++)
+            {
+                for (int y = 0; y < bitmap.Height; y++)
+                {
+                    Color pixel = bitmap.GetPixel(x, y);
+                    byte value = (byte)((306 * pixel.R + 601 * pixel.G + 117 * pixel.B) / 1024);
+                    grayscale[x + (y * bitmap.Height)] = value;
+                }
+            }
+            var average = grayscale.Average(x => x);
+            var bits = new BitArray(grayscale.Length);
+            foreach (var element in grayscale
+                .Select((value, index) => new { value, index })
+                .Where(x => x.value >= average))
+            {
+                bits.Set(element.index, true);
+            }
+            return bits;
+        }
         public static unsafe void ToThresholding(this Bitmap bitmap, bool reverse = false)
         {
             BitmapData bitmapData = null;
